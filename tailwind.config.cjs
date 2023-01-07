a71d1e15e0ae1ba34eaff64ab1c96a3447cba09e
Adria Martinez <adriamartinez94@gmail.com>
2023-01-07 21:36:13 +0100

    Add tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "background-color": "#0F0E17",
+        "primary-color": "#FF8906",
+        "secondary-color": "#FF9183",
+      },
+      boxShadow: {
+        "inner-shadow-card": "inset 0px 0px 16px #3A365B;",
+      },
+      borderColor: {
+        "border-color-personal-card":
+          "linear-gradient(90deg, #FF9183 0%, #D94A69 100%);",
+      },
+      fontFamily: {
+        "primary-font": "Open Sans, sans-serif",
+        "secondary-font": "JetBrains Mono, monospace;",
+      },
+    },
   },
   plugins: [],
 };
